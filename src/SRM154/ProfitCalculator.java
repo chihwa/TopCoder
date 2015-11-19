60a9e0366c4f619fe39dd6a9021b0fca36489824
Chi Hwa Kim <chihwa93@gmail.com>
2015-11-19 00:45:02 -0800

    11/19

---
+++ src/SRM154/ProfitCalculator.java
@@ -0,0 +1,25 @@
+package SRM154;
+
+/**
+ * Created by Chihwa on 11/18/2015.
+ */
+public class ProfitCalculator {
+    public int percent(String[] items) {
+
+        double cost = 0d;
+        double price = 0d;
+
+        for (String line : items) {
+            String[] prices = line.split(" ");
+            price += Double.parseDouble(prices[0]);
+            cost += Double.parseDouble(prices[1]);
+        }
+
+        return (int) ((price - cost) * 100 / price);
+    }
+
+    public static void main(String[] args) {
+        int r = new ProfitCalculator().percent(new String[]{"012.99 008.73", "099.99 050.00", "123.45 101.07"});
+        System.out.println(r);
+    }
+}
---
+++ src/SRM154/SuperRot.java
@@ -0,0 +1,29 @@
+package SRM154;
+
+/**
+ * Created by Chihwa on 11/18/2015.
+ */
+public class SuperRot {
+    public String decoder(String message) {
+        StringBuilder sb = new StringBuilder();
+        char input[] = message.toCharArray();
+        for (int i = 0; i < input.length; i++) {
+            char c = input[i];
+            int val;
+            if (c >= '0' && c <= '4') {
+                val = 5;
+            } else if (c >= '5' && c <= '9') {
+                val = -5;
+            } else if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M')) {
+                val = 13;
+            } else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z')) {
+                val = -13;
+            } else {
+                val = 0;
+            }
+            sb.append((char) (c + val));
+        }
+
+        return sb.toString();
+    }
+}
